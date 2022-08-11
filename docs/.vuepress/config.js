9362b14cec0fda4df1911ab0a97775406ff51430
fxbsuajy <fxbsujay@gmail.com>
2022-08-11 18:11:15 +0800

    初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'SUSU-OSS',
+    description: 'SUSU-OSS',
+    themeConfig: {
+        logo: '/logo.jpg',
+        nav: [
+            { text: '配置', link: '/config/' },
+            { text: '关于我', link: 'http://xuebin.xyz' }
+        ]
+    }
+}
